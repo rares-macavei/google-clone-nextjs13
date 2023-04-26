fa23940b7b78f5b800bb4ed0685e222d15043ba0
Rares Macavei <raresmacavei15@gmail.com>
2023-04-26 16:07:21 +0300

    api problem solving

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default async function WebSearchPage({searchParams}) {
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+    `https://www.googleapis.com/customsearch/v1?key=AIzaSyAkbx_oKnwZ-jkmrtSUe8yencGTdlFva7U&cx=c138859fd3df342f4&q=${searchParams.searchTerm}`
   )
 
   if(!response.ok) {
